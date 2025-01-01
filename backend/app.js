34747ea75fe9022625ed9ff7e2b3920353649cb1
EPZ/Enzo <enzo.polizzi.11@gmail.com>
2025-01-01 17:02:38 +0100

    Swagger first version without the action that do the app done

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,37 @@
+const swaggerUi = require('swagger-ui-express');
 const express = require ('express');
 const app = express();
 const port = 4956;
-const path = require('path')
+const path = require('path');
+const fs = require('fs')
+
+//Load of Swagger spec
+const swaggerDoc = JSON.parse(fs.readFileSync(path.join(__dirname,'swagger.json'),'utf-8'));
+
+//Serve uses Swagger UI 
+app.use('/swagger-docs',swaggerUi.serve,swaggerUi.setup(swaggerDoc));
+
+//To check if the Server works 
 app.get('/',(req,res) => {
     res.send("alive")
 })
-
-app.get('/course',(req,res) => {
+//Get method for the courses 
+app.get('/courses',(req,res) => {
     res.send("works")
 })
+app.get('/course/{:id}',(req,res) => {
+    res.send("get id works")
+})
+//Post Method to add new courses 
 app.post('/course', (req,res) =>{
     res.send('post works')
 })
-app.put('/course/:id', (req,res) => {
+//Update existing courses
+app.put('/course/{:id}', (req,res) => {
     res.send('put works')
 })
-app.delete('/course/:id', (req,res) => {
+//Delete existing courses 
+app.delete('/course/{:id}', (req,res) => {
     res.send('delete works')
 })
 
