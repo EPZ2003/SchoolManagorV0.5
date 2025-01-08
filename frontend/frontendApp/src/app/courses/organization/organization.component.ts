e2f0ce01dc19e7a28b02fab460e7fbb6474ee3e2
EPZ_2003 <enzo.polizzi.11@gmail.com>
2025-01-08 11:27:48 +0100

    Ag-Grid Works without the back

--- frontend/frontendApp/src/app/courses/organization/organization.component.ts
+++ frontend/frontendApp/src/app/courses/organization/organization.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import type { ColDef } from 'ag-grid-community';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
 
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
 @Component({
   selector: 'app-organization',
-  imports: [],
+  imports: [AgGridAngular],
   templateUrl: './organization.component.html',
   styleUrl: './organization.component.css'
 })
 export class OrganizationComponent {
+  rowData = [
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+];
 
+// Column Definitions: Defines the columns to be displayed.
+colDefs: ColDef[] = [
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" }
+];
+
+ngOnInit() {
+  this.rowData.push(    { make: "Toyota", model: "Corolla", price: 323322323, electric: false })
+}
 }
