21e37bb5a403c4f51acdae6c5b6d157b344c56e5
EPZ_2003 <enzo.polizzi.11@gmail.com>
2025-01-09 20:01:45 +0100

    ActivateRoute provider done

--- frontend/frontendApp/src/app/courses/organization/organization.component.spec.ts
+++ frontend/frontendApp/src/app/courses/organization/organization.component.spec.ts
@@ -1,8 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrganizationComponent } from './organization.component';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+
 //Work to do Because this Organization component use HttpClient modules by using apiService service
 /**
  * There is the the link for more details : https://angular.dev/guide/http/testing
@@ -14,11 +17,25 @@ describe('OrganizationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers : [
+        
+        //This i for ActivatedRoute
+        {
+          provide : ActivatedRoute,
+          useValue : {
+            // Mock static parameters
+            snapshot: {params : {id: '123'} },
+            // Mock observable query params
+            queryParams: of( {query : 'test'})
+          }
+        },
+        
         //This is for HttpClient Module
         provideHttpClient(),
         provideHttpClientTesting()
       ],
-      imports: [OrganizationComponent]
+      imports: [
+        OrganizationComponent,
+      ]
     })
     .compileComponents();
     //Creation of Backend Test
@@ -32,3 +49,5 @@ describe('OrganizationComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
