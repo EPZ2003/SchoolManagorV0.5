a8d335bf17c45c1412e63268be9a48b28e4605e0
EPZ_2003 <enzo.polizzi.11@gmail.com>
2025-01-09 19:45:51 +0100

    Default Jasmine test completed

--- frontend/frontendApp/src/app/courses/organization/creation-course/creation-course.component.spec.ts
+++ frontend/frontendApp/src/app/courses/organization/creation-course/creation-course.component.spec.ts
@@ -1,13 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreationCourseComponent } from './creation-course.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('CreationCourseComponent', () => {
   let component: CreationCourseComponent;
   let fixture: ComponentFixture<CreationCourseComponent>;
-
+//Work to do Because this Creation-Course component use HttpClient modules by using apiService service
+/**
+ * There is the the link for more details : https://angular.dev/guide/http/testing
+ */
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers : [
+        //This is for HttpClient Module
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ],
       imports: [CreationCourseComponent]
     })
     .compileComponents();
--- frontend/frontendApp/src/app/courses/organization/organization.component.spec.ts
+++ frontend/frontendApp/src/app/courses/organization/organization.component.spec.ts
@@ -1,17 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OrganizationComponent } from './organization.component';
-
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+//Work to do Because this Organization component use HttpClient modules by using apiService service
+/**
+ * There is the the link for more details : https://angular.dev/guide/http/testing
+ */
 describe('OrganizationComponent', () => {
   let component: OrganizationComponent;
   let fixture: ComponentFixture<OrganizationComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers : [
+        //This is for HttpClient Module
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ],
       imports: [OrganizationComponent]
     })
     .compileComponents();
-
+    //Creation of Backend Test
+    const HttpTesting = TestBed.inject(HttpTestingController)
     fixture = TestBed.createComponent(OrganizationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
