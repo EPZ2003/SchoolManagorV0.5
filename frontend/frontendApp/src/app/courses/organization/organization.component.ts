ca94dfb3bbce8d35ac1134c7f4839a1f501d60d3
EPZ_2003 <enzo.polizzi.11@gmail.com>
2025-01-09 14:40:59 +0100

    Style organization part done

--- frontend/frontendApp/src/app/courses/organization/organization.component.ts
+++ frontend/frontendApp/src/app/courses/organization/organization.component.ts
@@ -4,19 +4,23 @@ import type { ColDef } from 'ag-grid-community';
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
 import { ApiServiceService } from '../../api-service.service';
 import { Courses } from '../../models/Courses.dto';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 // Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule]);
 @Component({
   selector: 'app-organization',
-  imports: [AgGridAngular,RouterOutlet],
+  imports: [AgGridAngular,RouterOutlet,RouterLink],
   templateUrl: './organization.component.html',
   styleUrl: './organization.component.css'
 })
 export class OrganizationComponent {
   //Injection with the service that comunicates with the backend
   private readonly apiService:ApiServiceService = inject(ApiServiceService);
+
+  //For the toogleButton
+  isActive:boolean = false;
+  toCreate:string = "creation";
 
   //Creation of 3 rowData for 3 differents tables with the same colDefs
   rowDataModA:Courses[] = [];
@@ -43,6 +47,11 @@ export class OrganizationComponent {
       },
       error: err => console.error('Error fetching courses:', err)
     });
+  }
+
+  //Toggle the string that enable or back the initial subpages 
+  toggleCreation() {
+  
   }
 
 }
