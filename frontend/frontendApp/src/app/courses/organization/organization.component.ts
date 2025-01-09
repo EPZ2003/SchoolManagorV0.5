a8038afa8fde6d3ea95a7e8dd24e50690e2d6481
EPZ_2003 <enzo.polizzi.11@gmail.com>
2025-01-09 14:51:49 +0100

    Minimal writing changement

--- frontend/frontendApp/src/app/courses/organization/organization.component.ts
+++ frontend/frontendApp/src/app/courses/organization/organization.component.ts
@@ -42,8 +42,7 @@ export class OrganizationComponent {
         //Using filter for each table and choose the good module for each one (ex: ) 
         this.rowDataModA = data.filter(item => item.module =="algebra"); 
         this.rowDataModB = data.filter(item => item.module =="science");
-        //this.rowDataModC = data.filter(item => item.module =="physics"); 
-        this.rowDataModC = data; 
+        this.rowDataModC = data.filter(item => item.module =="physics"); 
       },
       error: err => console.error('Error fetching courses:', err)
     });
